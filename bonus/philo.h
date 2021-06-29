6c0e3564639c00d6331c7a648bc3fec7ed39a678
Gharbaoui <mohamed9gharbaoui@gmail.com>
2021-06-29 13:15:19 +0100

    working

--- bonus/philo.h
+++ bonus/philo.h
@@ -6,7 +6,7 @@
 /*   By: mel-ghar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 12:23:24 by mel-ghar          #+#    #+#             */
-/*   Updated: 2021/06/29 12:40:46 by mel-ghar         ###   ########.fr       */
+/*   Updated: 2021/06/29 12:53:16 by mel-ghar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,22 @@
 
 typedef struct s_philo
 {
-    int     			id;
-    unsigned long long	td;
-    long				te;
-    long				ts;
-    unsigned long long	start_time;
-    int     			max_meals;
-    int     			meals;
-    char    			status;
-    unsigned long long	last_time_eat;
-    sem_t   			*s_print;
-    sem_t   			*s_forks;
-    sem_t   			*s_done_eat;
+	int					id;
+	unsigned long long	td;
+	long				te;
+	long				ts;
+	unsigned long long	start_time;
+	int					max_meals;
+	int					meals;
+	char				status;
+	unsigned long long	last_time_eat;
+	sem_t				*s_print;
+	sem_t				*s_forks;
+	sem_t				*s_done_eat;
 	sem_t				*s_died;
 }						t_philo;
 
-typedef	struct	s_sem_info
+typedef struct s_sem_info
 {
 	int		size;
 	sem_t	**sem_eat;
@@ -57,7 +57,7 @@ typedef struct s_info
 	int		te;
 	int		ts;
 	int		meals;
-}	t_info;
+}			t_info;
 
 long				help_ft_atoi(const char *str, int np);
 void				ft_putchar(char c);
@@ -72,9 +72,11 @@ int					is_number(char *num);
 int					non_number(char **nums, int size);
 int					collect_data(int size, char **nums, t_info *info);
 int					start_simulation(t_info *info, int *pids);
-t_philo				**get_philos(t_info info, sem_t *s_p, sem_t *forks, sem_t *s_died);
+t_philo				**get_philos(t_info info, sem_t *s_p,
+						sem_t *forks, sem_t *s_died);
 t_philo				*get_one_philo(t_info info, int id);
-void				start_philos(t_philo **philos, int *pids, t_info info, int num);
+void				start_philos(t_philo **philos, int *pids,
+						t_info info, int num);
 void				life_cycle_of_philo(t_philo *philo);
 void				print_info(t_philo *ph);
 long				get_time(void);
@@ -91,7 +93,8 @@ void				*see_all(void *data);
 void				die_msg(t_philo *ph);
 void				ft_usleep(long delay);
 void				check_if_hes_done(t_philo *ph);
-void				check_all_proccess(t_philo **philos, int *pids, t_info info);
+void				check_all_proccess(t_philo **philos,
+						int *pids, t_info info);
 t_sem_info			*get_direct_access(t_philo **phs, t_info info, int *pids);
 void				kill_all(int *pids, int size);
 sem_t				*get_sem(char *name, int value);
